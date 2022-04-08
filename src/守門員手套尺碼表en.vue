fb3c2da3393780beb860226e0dc6a4c8781c7554
Labbott <1006762873@qq.com>
2022-04-08 12:24:27 +0800

    守門員手套尺碼表

---
+++ src/守門員手套尺碼表en.vue
@@ -0,0 +1,141 @@
+<!--
+ * @Description: 守門員手套尺碼表
+ * @Author: lijiapeng
+ * @Date: 2022-04-08 12:22:14
+ * @LastEditTime: 2022-04-08 12:23:40
+ * @LastEditors: lijiapeng
+ * @Reference: 
+-->
+
+<template>
+  <div class="content">
+    <div class="top-title">
+      <div class="sizeClose">
+        <i id="closeDrawer" class="iconfont iconcross-small"></i>
+      </div>
+      <div class="text">Goalkeepers' Gloves Size Chart</div>
+    </div>
+    <div class="type">
+      <!-- <div id="left-type" class="left active">尺碼表</div> -->
+      <!-- <div id="right-type" class="right">如何測量</div> -->
+      <div id="left-type" class="left active">SIZE & FIT</div>
+      <div id="right-type" class="right">How to Measure</div>
+    </div>
+    <div class="table-info">
+      <div class="size-info" id="sizeInfo">
+        <div class="size-content">
+          <div class='size-chart'>
+            <table> 
+              <tbody> 
+                <tr class="tr-color tr-title"> 
+                  <td><span>Size</span></td> 
+                  <td><span>Hand Measurement (cm)</span></td> 
+                  <td><span>Specifications</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>4</span></td> 
+                  <td rowspan="2"><span>14 or less</span></td> 
+                  <td><span>210/120</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>4.5</span></td> 
+                  <td><span>220/125</span></td> 
+                </tr> 
+                  <tr class="tr-color"> 
+                  <td><span>5</span></td> 
+                  <td rowspan="2"><span>14 - 15</span></td> 
+                  <td><span>220/125</span></td> 
+                </tr> 
+                <tr class="tr-color"> 
+                  <td><span>5.5</span></td> 
+                  <td><span>230/125</span></td> 
+                </tr> 
+                  <tr class=""> 
+                  <td><span>6</span></td> 
+                  <td rowspan="2"><span>15 - 18</span></td> 
+                  <td><span>230/125</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>6.5</span></td> 
+                  <td><span>230/130</span></td> 
+                </tr> 
+                  <tr class="tr-color"> 
+                  <td><span>7</span></td> 
+                  <td rowspan="2"><span>18 - 20</span></td> 
+                  <td><span>210/120</span></td> 
+                </tr> 
+                <tr class="tr-color"> 
+                  <td><span>7.5</span></td> 
+                  <td><span>220/125</span></td> 
+                </tr> 
+                  <tr class=""> 
+                  <td><span>8</span></td> 
+                  <td rowspan="2"><span>20 - 22</span></td> 
+                  <td><span>220/12</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>8.5</span></td> 
+                  <td><span>230/125</span></td> 
+                </tr> 
+                  <tr class="tr-color"> 
+                  <td><span>9</span></td> 
+                  <td rowspan="2"><span>22 - 24</span></td> 
+                  <td><span>230/125</span></td> 
+                </tr> 
+                <tr class="tr-color"> 
+                  <td><span>9.5</span></td> 
+                  <td><span>230/130</span></td> 
+                </tr> 
+                  <tr class=""> 
+                  <td><span>10</span></td> 
+                  <td rowspan="2"><span>24 - 27</span></td> 
+                  <td><span>230/130</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>10.5</span></td> 
+                  <td><span>240/135</span></td> 
+                </tr> 
+                  <tr class="tr-color"> 
+                  <td><span>11</span></td> 
+                  <td rowspan="2"><span>27 - 29</span></td> 
+                  <td><span> -</span></td> 
+                </tr> 
+                <tr class="tr-color"> 
+                  <td><span>11.5</span></td> 
+                  <td><span> -</span></td> 
+                </tr> 
+                  <tr class=""> 
+                  <td><span>12</span></td> 
+                  <td rowspan="2"><span>29 - 32</span></td> 
+                  <td><span> -</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>12.5</span></td> 
+                  <td><span> -</span></td> 
+                </tr> 
+              </tbody> 
+            </table>
+          </div>
+        </div>
+        <div class="tips">
+          <div class="tips-title">Fit Tips：</div>
+          <div class="tips-content">Please note that the size chart refers to the measurement of oneself, but not the clothes itself.</div>
+          <div class="tips-content">If you're on the borderline between two sizes, please order the smaller size for a tighter fit or the larger size for a looser fit.</div>
+        </div>
+      </div>
+      <div class="size-measure" id="measure">
+        <div class="measure">
+          <div class="img-info">
+            <img class="img-pc" src="https://img.adidas.com.hk/resources/images/size/men.png" alt="" srcset="">
+            <img class="img-mob" src="https://img.adidas.com.hk/resources/images/size/menMeasure.jpg" alt="" srcset="">
+          </div>
+        </div>
+        <div class="tips">
+          <div class="tips-content">
+            How to measure detail how to measure detail how to measure detail How to measure detail how to measure detail how to measure detailHow to measure detail how to measure detail how to measure detail How to measure detail how to measure detail how to measure detail
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
