7ae6352f20b0a63c1f40ba8a22e1c9ec55c70bb6
Labbott <1006762873@qq.com>
2022-04-08 11:59:52 +0800

    護腿板通用尺碼表

---
+++ src/護腿板通用尺碼表en.vue
@@ -0,0 +1,67 @@
+<!--
+ * @Description: 護腿板通用尺碼表
+ * @Author: lijiapeng
+ * @Date: 2022-04-08 11:55:23
+ * @LastEditTime: 2022-04-08 11:59:36
+ * @LastEditors: lijiapeng
+ * @Reference: 
+-->
+
+<template>
+  <div class="content">
+    <div class="top-title">
+      <div class="sizeClose">
+        <i id="closeDrawer" class="iconfont iconcross-small"></i>
+      </div>
+      <div class="text">Shin Pads Size Chart</div>
+    </div>
+    <div class="type">
+      <!-- <div id="left-type" class="left active">尺碼表</div> -->
+      <!-- <div id="right-type" class="right">如何測量</div> -->
+      <div id="left-type" class="left active">SIZE & FIT</div>
+      <div id="right-type" class="right">How to Measure</div>
+    </div>
+    <div class="table-info">
+      <div class="size-info" id="sizeInfo">
+        <div class="size-content">
+          <div class='size-chart'>
+            <table> 
+              <tbody> 
+                <tr class="tr-color tr-title"> 
+                  <td><span>Size</span></td> 
+                  <td><span>S</span></td> 
+                  <td><span>M</span></td> 
+                  <td><span>L</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>Height Reference</span></td> 
+                  <td><span>100-120</span></td> 
+                  <td><span>120-140</span></td> 
+                  <td><span>140-160</span></td> 
+                </tr> 
+              </tbody> 
+            </table>
+          </div>
+        </div>
+        <div class="tips">
+          <div class="tips-title">Fit Tips：</div>
+          <div class="tips-content">Please note that the size chart refers to the measurement of oneself, but not the clothes itself.</div>
+          <div class="tips-content">If you're on the borderline between two sizes, please order the smaller size for a tighter fit or the larger size for a looser fit.</div>
+        </div>
+      </div>
+      <div class="size-measure" id="measure">
+        <div class="measure">
+          <div class="img-info">
+            <img class="img-pc" src="https://img.adidas.com.hk/resources/images/size/men.png" alt="" srcset="">
+            <img class="img-mob" src="https://img.adidas.com.hk/resources/images/size/menMeasure.jpg" alt="" srcset="">
+          </div>
+        </div>
+        <div class="tips">
+          <div class="tips-content">
+            How to measure detail how to measure detail how to measure detail How to measure detail how to measure detail how to measure detailHow to measure detail how to measure detail how to measure detail How to measure detail how to measure detail how to measure detail
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
