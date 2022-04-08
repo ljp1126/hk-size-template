82a514eb09ce0a52ca621e20554c2586de1ae29f
Labbott <1006762873@qq.com>
2022-04-08 13:23:08 +0800

    帽子和袜子

---
+++ src/帽子尺碼表hk.vue
@@ -0,0 +1,95 @@
+<!--
+ * @Description: 
+ * @Author: lijiapeng
+ * @Date: 2022-04-08 12:56:50
+ * @LastEditTime: 2022-04-08 12:56:51
+ * @LastEditors: lijiapeng
+ * @Reference: 
+-->
+
+<template>
+  <div class="content">
+    <div class="top-title">
+      <div class="sizeClose">
+        <i id="closeDrawer" class="iconfont iconcross-small"></i>
+      </div>
+      <div class="text">帽子尺碼表</div>
+    </div>
+    <div class="type">
+      <div id="left-type" class="left active">尺碼表</div>
+      <div id="right-type" class="right">如何測量</div>
+      <!-- <div id="left-type" class="left active">SIZE & FIT</div> -->
+      <!-- <div id="right-type" class="right">How to Measure</div> -->
+    </div>
+    <div class="table-info">
+      <div class="size-info" id="sizeInfo">
+        <div class="size-content">
+          <div class='size-chart'>
+            <table> 
+              <tbody> 
+                <tr class="tr-color tr-title"> 
+                  <td><span>Size</span></td> 
+                  <td><span>Width (Inch)</span></td> 
+                  <td><span>Width (cm)</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>Infants</span></td> 
+                  <td><span>16.5"</span></td> 
+                  <td><span>42</span></td> 
+                </tr> 
+                <tr class="tr-color"> 
+                  <td><span>Toddlers</span></td> 
+                  <td><span>18.9" - 19.7"</span></td> 
+                  <td><span>42 - 50</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>Children</span></td> 
+                  <td><span>19.7" - 20.5"</span></td> 
+                  <td><span>50 - 52</span></td> 
+                </tr> 
+                <tr class="tr-color"> 
+                  <td><span>Youth</span></td> 
+                  <td><span>20.5" - 21.3"</span></td> 
+                  <td><span>52 - 54</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>Women (S/M)</span></td> 
+                  <td><span>21.3" - 22.0"</span></td> 
+                  <td><span>54 - 56</span></td> 
+                </tr> 
+                <tr class="tr-color"> 
+                  <td><span>Men (M/L)</span></td> 
+                  <td><span>22.0" - 22.8"</span></td> 
+                  <td><span>56 - 58</span></td> 
+                </tr> 
+                <tr class=""> 
+                  <td><span>Large (L/XL)</span></td> 
+                  <td><span>22.8" - 23.6"</span></td> 
+                  <td><span>58 - 60</span></td> 
+                </tr> 
+              </tbody> 
+            </table>
+          </div>
+        </div>
+        <div class="tips">
+          <div class="tips-title">Fit Tips：</div>
+          <div class="tips-content">Please note that the size chart refers to the measurement of oneself, but not the clothes itself.</div>
+          <div class="tips-content">If you're on the borderline between two sizes, please order the smaller size for a tighter fit or the larger size for a looser fit.</div>
+        </div>
+      </div>
+      <div class="size-measure" id="measure">
+        <div class="measure">
+          <div class="img-info">
+            <img class="img-pc" src="https://img.adidas.com.hk/resources/images/size/men.png" alt="" srcset="">
+            <img class="img-mob" src="https://img.adidas.com.hk/resources/images/size/menMeasure.jpg" alt="" srcset="">
+          </div>
+        </div>
+        <div class="tips">
+          <div class="tips-content">
+            How to measure detail how to measure detail how to measure detail How to measure detail how to measure detail how to measure detailHow to measure detail how to measure detail how to measure detail How to measure detail how to measure detail how to measure detail
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
